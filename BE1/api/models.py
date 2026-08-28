43f08320157776ea6a46044241c5188d4c4fbd92
Caio Pereira <caio.dev.person@gmail.com>
2026-08-28 16:55:14 -0300

    la ele

--- BE1/api/models.py
+++ BE1/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=255)
+    ativa = models.BooleanField(default=True)
+
+    def __str___(self):
+        return self.nome
